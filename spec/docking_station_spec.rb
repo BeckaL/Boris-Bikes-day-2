25f1bf02847a6e69ecd2d619cca160b058d5add6
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-10-03 16:25:08 +0100

    fixed spec tests so no errors raised

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -15,14 +15,17 @@ describe DockingStation do
   end
 
   it 'gets a bike and expects it to be working' do
-    bike = DockingStation.new.release_bike
+    ds = DockingStation.new
+    ds.rack.push(Bike.new)
+    bike = ds.release_bike
     expect(bike.working?).to eq true
   end
 
   it 'docks bike' do
     ds = DockingStation.new
     bike = Bike.new
-    expect(ds.dock(bike)).to eq true
+    ds.dock(bike)
+    expect(ds.rack.length).to eq 1
   end
 
   it 'does not dock bike when rack is full' do
@@ -30,7 +33,7 @@ describe DockingStation do
     bike = Bike.new
     ds.dock(bike)
     second_bike = Bike.new
-    expect { ds.dock(second_bike) }.to raise_error
+    expect { ds.dock(second_bike) }.to raise_error 'Rack full'
   end
 
 
