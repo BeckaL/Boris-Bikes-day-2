1ba0c11f3945ca6042e778095f854794d2a7b2aa
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-10-03 15:00:23 +0100

    made DS release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,12 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it 'responds to release bike' do
     expect(DockingStation.new).respond_to?(:release_bike)
+  end
+  it 'gets a bike and expects it to be working' do
+    bike = DockingStation.new.release_bike
+    expect(bike.working?).to eq true
   end
 end
