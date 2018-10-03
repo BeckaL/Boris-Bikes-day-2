90729a892a5c4b99adacd5fa2b4e354b1755b110
Becka <rebdok@hotmail.com>
2018-10-03 15:15:46 +0100

    used instance variables for docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,17 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader  :rack
+
+  def initialize
+    @rack = []
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def dock(bike)
+    true
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,23 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
+
   it 'responds to release bike' do
     expect(DockingStation.new).respond_to?(:release_bike)
   end
+
   it 'gets a bike and expects it to be working' do
     bike = DockingStation.new.release_bike
     expect(bike.working?).to eq true
   end
+
+  it 'docks bike' do
+    ds = DockingStation.new
+    bike = Bike.new
+
+    expect(ds.dock(bike)).to eq true
+
+  end
+
+
 end
