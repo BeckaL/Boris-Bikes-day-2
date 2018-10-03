c3179d7210f7c73dee722a7776b843103d503f20
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-10-03 18:13:05 +0100

    finished chapter 18 everything working great

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -14,14 +14,13 @@ class DockingStation
     fail 'There are no bikes available at this station' unless !(@rack.empty?)
     working_bikes = @rack.map.select { |bike| bike.working? }
     fail 'No working bikes available' unless !(working_bikes.empty?)
-    # Need to remember to come back to this to fix for multiple bikes
-    working_bikes.pop
+    @rack.delete(working_bikes.first)
   end
 
   def dock(bike)
     fail 'Rack full' unless !full?
     @rack.push(bike)
-    return 'Broken bike has been reported' if !(bike.working?) 
+    return 'Broken bike has been reported' if !(bike.working?)
   end
 
 private
